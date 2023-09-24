ef25c74ce78e46e26e2e9780b20ab8c4669e1061
Bidyasagar <bidyasagarhazarika@gmail.com>
2023-09-25 02:54:31 +0530

    Passport

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express();
@@ -15,10 +16,21 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use(session({
+  secret:"Bidyasagar Hazarika",
+  resave:false,
+  saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/userDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+// mongoose.set("useCreateIndex",true);
 
 // Define the user schema
 const userSchema = new mongoose.Schema({
@@ -26,12 +38,14 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-
-
-
-
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get('/', function (req, res) {
   res.render('home');
