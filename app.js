2df2060703dcb786e572d4b2ee9c053ce0ea9941
Bidyasagar <bidyasagarhazarika@gmail.com>
2023-09-25 23:57:13 +0530

    try

--- app.js
+++ app.js
@@ -60,69 +60,11 @@ app.get('/register', function (req, res) {
 });
 
 app.post('/register', function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (!username || !password) {
-    const alertMessage = 'Both username and password are required.';
-    return res.send(`<script>alert("${alertMessage}"); window.location.href="/register";</script>`);
-  }
-
-  bcrypt.hash(password, saltRounds, function (err, hash) {
-    if (err) {
-      console.error(err);
-      const alertMessage = 'An error occurred during registration.';
-      return res.send(`<script>alert("${alertMessage}"); window.location.href="/register";</script>`);
-    }
-
-    const newUser = new User({
-      email: username,
-      password: hash,
-    });
-
-    newUser
-      .save()
-      .then(() => {
-        res.render('secrets');
-      })
-      .catch((err) => {
-        console.error(err);
-        const alertMessage = 'An error occurred during registration.';
-        res.send(`<script>alert("${alertMessage}"); window.location.href="/register";</script>`);
-      });
-  });
+  
 });
 
 app.post('/login', function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (!username || !password) {
-    const alertMessage = 'Both username and password are required.';
-    return res.send(`<script>alert("${alertMessage}"); window.location.href="/login";</script>`);
-  }
-
-  User.findOne({ email: username })
-    .then((foundUser) => {
-      if (!foundUser) {
-        const alertMessage = 'User not found.';
-        return res.send(`<script>alert("${alertMessage}"); window.location.href="/login";</script>`);
-      }
-
-      bcrypt.compare(password, foundUser.password, function (err, result) {
-        if (err || !result) {
-          const alertMessage = 'Incorrect password.';
-          return res.send(`<script>alert("${alertMessage}"); window.location.href="/login";</script>`);
-        }
-
-        res.render('secrets');
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      const alertMessage = 'An error occurred during login.';
-      res.send(`<script>alert("${alertMessage}"); window.location.href="/login";</script>`);
-    });
+  
 });
 
 
